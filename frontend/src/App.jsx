85bad186e2c0d07f1d7bf846b4b57b4b08d95f3d
DosSantosBrunoo <bruno.dossantos@anima.edu.uy>
2021-08-05 18:28:16 -0300

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import './App.css';
 import React from 'react';
-import Inicio from './components/Inicio';
 import Get from './components/Get';
 
 function App() {
   return (
     <div className="App">
-      <Inicio />
       <Get />
     </div>
   );
