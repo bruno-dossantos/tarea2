28b27f29905684df726d2f24e3922d320487e5dd
DosSantosBrunoo <bruno.dossantos@anima.edu.uy>
2021-08-06 16:09:33 -0300

    update front

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,14 +4,12 @@ import React from 'react';
 import Get from './components/Get';
 import Post from './components/Post';
 import Navbar from './components/Navbar';
-import Inicio from './components/Inicio';
 import Show from './components/Show';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Inicio />
       <Get />
       <Show />
       <Post />
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-default position-fixed">
+    <nav className="navbar navbar-expand-lg navbar-default position-fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="./Inicio.jsx">
+        <a className="navbar-brand" href=".">
           Movies
         </a>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
