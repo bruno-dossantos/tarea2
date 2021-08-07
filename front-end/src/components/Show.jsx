307e235e286ef78e33003271bc4bed71f8d21f0e
Ezequiel Padilla <ezequiel.padilla.m@gmail.com>
2021-08-06 22:44:40 -0300

    Feat add show method

--- front-end/src/components/Show.jsx
+++ front-end/src/components/Show.jsx
@@ -1,46 +1,69 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default class Show extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function Show() {
+  const [movies, setMovie] = useState([]);
+  const [id, setId] = useState('');
+  const [error, setError] = useState('');
 
-  componentDidMount() {
-    document.title = 'Get movie';
-  }
+  const getMovie = async () => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/pelicula/${id}`);
+      setMovie(response.data.data);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('fallo axios', err);
+      setError('Hubo un error al traer el pais');
+    }
+  };
 
-  render() {
-    return (
-      <div id="2" className="section container-fluid">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-6 container-form">
-            <form className="form-group" action="">
-              <h1
-                style={{ textAlign: 'center' }}
-                className="d-flex flex-row justify-content-center"
-              >
-                Get movie
-              </h1>
-              <input
-                autoComplete="off"
-                required
-                type="number"
-                placeholder="ID"
-                className="form-control"
-                name="idMovie"
-              />
-              {' '}
-              <br />
-              <button type="submit" className="btn">
-                Ready
-              </button>
-              <a style={{ marginLeft: '10px' }} href="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49AC21526EAD9DC631160FC842FAC9F6557C70439E0793BB4D6C6BFFDB2DD68A/scale?width=1200&aspectRatio=1.78&format=jpeg" target="_blank" rel="noreferrer">Cars 2</a>
-            </form>
-          </div>
+  const handleChange = async (event) => {
+    const e = event.target.value;
+    setId(e);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getMovie();
+    setId('');
+  };
+
+  return (
+    <div id="2" className="section container-fluid">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-6 container-form">
+          <form className="form-group" action="" onSubmit={handleSubmit}>
+            <h1 style={{ textAlign: 'center' }} className="d-flex flex-row justify-content-center">
+              Get movie
+            </h1>
+            <input
+              autoComplete="off"
+              required
+              type="number"
+              placeholder="ID"
+              className="form-control"
+              name="idMovie"
+              onChange={handleChange}
+            />
+            {' '}
+            <br />
+            <button type="submit" className="btn">
+              Ready
+            </button>
+            {error}
+            {movies.map((movie) => (
+              <a style={{ marginLeft: '10px' }} key={movie.idPelicula} href={movie.img} target="_blank" rel="noreferrer">{movie.nombre}</a>
+            ))}
+            {/* <a style={{ marginLeft: '10px' }} href={movie} target="_blank" rel="noreferrer">
+              Image
+            </a> */}
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Show;
